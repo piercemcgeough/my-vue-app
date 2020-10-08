34518e5e9cf5d8ba037302739ca7f13ac82ce879
Pierce McGeough <pmcgeough@hotmail.co.uk>
2020-10-08 17:00:28 +0100

    typo

--- src/views/AmongUs.vue
+++ src/views/AmongUs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="among">
-    <h1>This is an amoung us page</h1>
+    <h1>This is an amoug us page</h1>
 
     <img
       src="https://cdn.mos.cms.futurecdn.net/93GAa4wm3z4HbenzLbxWeQ-1200-80.jpg"
