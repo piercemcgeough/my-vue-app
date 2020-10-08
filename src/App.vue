09f50d070e28c0e4515e881f05debf75dbe3dd5d
Pierce McGeough <pmcgeough@hotmail.co.uk>
2020-10-08 16:45:03 +0100

    Among us

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/among-us">Among Us!</router-link>
     </div>
+
     <router-view />
   </div>
 </template>
